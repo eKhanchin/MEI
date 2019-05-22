7583e8d6f48b1e5db25e6197087f280ec09712d9
Max <49946852+CkzKaktooZ@users.noreply.github.com>
2019-05-22 23:43:04 +0300

    Add files via upload
    This is all i have, almost

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MEIWebsite2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contacts()
+        {
+            return View();
+        }
+
+        public ActionResult AltContacts()
+        {
+            return View();
+        }
+
+        public ActionResult Artists()
+        {
+            return View();
+        }
+
+        public ActionResult Albums()
+        {
+            return View();
+        }
+
+        public ActionResult Requests()
+        {
+            return View();
+        }
+    }
+}
